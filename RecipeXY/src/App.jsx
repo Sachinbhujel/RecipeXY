b4f78bcdd4761f69bfa92a9b829b8f37b2eb5389
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-04-26 08:16:22 +0000

    commit

--- RecipeXY/src/App.jsx
+++ RecipeXY/src/App.jsx
@@ -1,17 +1,29 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './Home';
-import Recipe_Detail from './Recipe_Detail';
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    BrowserRouter,
+} from "react-router-dom";
+import Home from "./Home";
+import Recipe_Detail from "./Recipe_Detail";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<div className='main'><Home /></div>} />
-        <Route path='/recipe/:id' element={<Recipe_Detail />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div className="main">
+                            <Home />
+                        </div>
+                    }
+                />
+                <Route path="/recipe/:id" element={<Recipe_Detail />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
